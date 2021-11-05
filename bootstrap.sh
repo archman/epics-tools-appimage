01f9be8f5cb574df59f09d5d56bc011acbda042e
Tong Zhang <zhangt@frib.msu.edu>
2021-11-05 16:18:52 -0400

    ENH: Recipe for building AppImages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash -i
+
 #
 # Build AppImages from EPICS base.
 #
@@ -6,121 +7,92 @@
 #
 set -Eeuo pipefail
 trap cleanup SIGINT SIGTERM ERR EXIT
-TMPDIRS=()
-
-function cleanup {
+cleanup() {
   trap - SIGINT SIGTERM ERR EXIT
-  for d in "${TMPDIRS[@]}"
-  do
-    rm -rf $d
-  done
+  set +u
+  rm -rf $appdir_path_var > /dev/null
 }
 
-# Other initialization before building AppImages
-function init {
+# initialization before building AppImages
+init() {
     echo "Initialization..."
+    #exe_name="softIocPVA"
+    exe_name="caget caput camonitor cainfo softIoc"
+    exe_name+=" pvget pvput pvmonitor pvinfo pvcall p2p softIocPVA"
+    # combined=false # each exe to one AppImage
+    combined=true # all exes to a single AppImage
+    base_ver="7.0.6.1"
+    cwdir="."
+    base_path="${cwdir}/base-${base_ver}"
+    appdir_path="AppDir"
+    packages_to_install=""
 }
 
-# Install additional dependencies, $@
-function install-packages {
-    echo "Install Debian packages..."
-    ! [[ $# -eq 0 ]] && apt-get -y install "$@" || return 0
-}
-
-#
-# Get EPICS base source code
-# artifacts generated show up in the current directory
-function get-epics-base {
-    echo "Get EPICS base version ${ver}..."
-    ! [[ -e base-${ver} ]] && wget --no-check-certificate \
-        https://epics.anl.gov/download/base/base-${ver}.tar.gz && \
-        tar xvf base-${ver}.tar.gz || return 0
-}
-
-# Build EPICS base
-function build-epics-base {
-    echo "Build EPICS base..."
-    cd base-${ver}
-    make -j4
-    cd ../
-}
-
-# Create AppImage,
-# $1: executable name, $2: desktop entry path (optional), $3: icon path (optional)
-function create-appimage {
-    echo "Create AppImage for $1..."
-    exe_path=$(find base-${ver}/bin -name $1 -exec readlink -f {} \;)
-    app_dir_name="AppDir-$1"
-    desktop_path=${2:-}
-    icon_path=${3:-"/tmp/tux.png"}
-    if [ -z ${desktop_path} ]; then
-        linuxdeploy --executable ${exe_path} \
-                    --icon-file ${icon_path} \
-                    --icon-filename=$1 \
-                    --create-desktop-file \
-                    --appdir ${app_dir_name}
-    else
-        icon_name=$(basename ${icon_path})
-        sed -i /Icon/"s|.*|Icon=${icon_name%%.*}|" ${desktop_path}
-        linuxdeploy --executable ${exe_path} \
-                    --icon-file ${icon_path} \
-                    --desktop-file ${desktop_path} \
-                    --appdir ${app_dir_name}
-    fi
+# Modified AppDir, if needed.
+# $1: executable name, $2: base path $3: path of AppDir
+patch-appdir(){
     # app-wise recipe
+    local basedir=$2
+    local appdir=$3
     case "${1-}" in
         softIoc)
-            recipe-softIoc ${app_dir_name}
+            recipe-softIoc $basedir $appdir
             ;;
         softIocPVA)
-            recipe-softIocPVA ${app_dir_name}
+            recipe-softIocPVA $basedir $appdir
             ;;
     esac
-    linuxdeploy --appdir ${app_dir_name} --output appimage
 }
 
 # Additional work needs done for creating AppImage for softIoc
-function recipe-softIoc {
-    local app_dir_name=$1
-    mkdir -p ${app_dir_name}/dbd
-    cp base-${ver}/dbd/softIoc.dbd ${app_dir_name}/dbd
+# $1: base path, $2: AppDir path
+recipe-softIoc() {
+    mkdir -p $2/dbd
+    cp $1/dbd/softIoc.dbd $2/dbd
 }
 
-# additional work needs done for creating AppImage for softIocPVA
-function recipe-softIocPVA {
-    local app_dir_name=$1
-    mkdir -p ${app_dir_name}/dbd
-    cp base-${ver}/dbd/softIocPVA.dbd ${app_dir_name}/dbd
+# Additional work needs done for creating AppImage for softIocPVA
+# $1: base path, $2: AppDir path
+recipe-softIocPVA() {
+    mkdir -p $2/dbd
+    cp $1/dbd/softIocPVA.dbd $2/dbd
 }
 
-## Real work started below...
-## Initialization
-init
+# build one appimage
+build-appimage() {
+    ## Real work started below...
+    ## Initialization
+    init
 
-## Install additional pacakge via apt-get
-# install-packages pkg-name1 pkg-name2 ...
-install-packages
+    ## Install additional pacakge via apt-get
+    # install-packages pkg-name1 pkg-name2 ...
+    #install-packages $packages_to_install
 
-## grab the source if does not exist
-ver="7.0.6.1"
-get-epics-base
+    ## grab the source if does not exist
+    #get-epics-base $base_ver $cwdir
 
-## build epics base
-build-epics-base
+    ## build epics base
+    #build-epics-base $base_path
 
-## create one AppImage at a time
-# create-appimage softIoc desktop-entries/softIoc.desktop desktop-entries/tux1.png
-# create-appimage caget
+    # create AppImage(s)
+    if ! [[ "$combined" = true ]]; then
+        appdir_path=""
+        for app in $exe_name; do
+            create-appdir $base_path $app $appdir_path && \
+                appdir_path_var=$(get-appdir $app) && \
+                patch-appdir ${app} ${base_path} ${appdir_path_var} && \
+                generate-appimage ${appdir_path_var} &
+        done
+        wait
+    else
+            create-appdir $base_path $app $appdir_path
+            appdir_path_var=$(get-appdir)
+            patch-appdir ${app} ${base_path} ${appdir_path_var}
+            generate-appimage ${appdir_path_var}
+        echo "Build all ELFs into a single one AppImage."
+    fi
+}
 
-#all_elfs=$(find base-${ver}/bin/ -type f -exec file {} \; | grep ELF \
-#           | awk -F':' '{print $1}' | awk -F'/' '{print $NF}' | xargs)
-#exe_list=${all_elfs}
+#
+build-appimage
 
-## create multiple AppImages at the same time
-exe_list="caget caput camonitor cainfo softIoc"
-exe_list+=" pvget pvput pvmonitor pvinfo pvcall p2p softIocPVA"
-for app_name in ${exe_list}; do
-    TMPDIRS+=("AppDir-$app_name")
-    create-appimage ${app_name} &
-done
-wait
