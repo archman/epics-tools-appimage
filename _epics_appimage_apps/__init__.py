3bf67845adeb52e73afa56cb50d48553ec008b23
Tong Zhang <zhangt@frib.msu.edu>
2021-12-08 09:33:25 -0500

    REF: Launch apps from epics-base-tools global entry point.

--- _epics_appimage_apps/__init__.py
+++ _epics_appimage_apps/__init__.py
@@ -6,20 +6,27 @@ import pathlib
 import subprocess
 import sys
 
+# global entry point
+LAUNCHER_NAME = 'epics-base-tools'
+
 PKG_DIR = pathlib.Path(__file__).parent.parent
 APP_NAME_PATH_MAP = {
     f.name.rsplit('-', 1)[0]: f.resolve()
-    for f in PKG_DIR.glob("**/*.AppImage")
+    for f in PKG_DIR.glob("AppImages/*.AppImage")
 }
+
+r = subprocess.run(APP_NAME_PATH_MAP[LAUNCHER_NAME], capture_output=True, text=True)
+# a list of app names which could be launched from the global entry point
+SUPPORT_APP_LIST = r.stdout.strip().split()
 
 
 class AppRunner(object):
     def __init__(self, app_name):
-        self._fn = APP_NAME_PATH_MAP[app_name]
+        self._cmdlist = [APP_NAME_PATH_MAP[LAUNCHER_NAME], app_name]
 
     def __call__(self):
         """Run an AppImage."""
-        cmd = [self._fn] + sys.argv[1:]
+        cmd = self._cmdlist + sys.argv[1:]
         subprocess.run(cmd, stderr=subprocess.STDOUT)
 
 
