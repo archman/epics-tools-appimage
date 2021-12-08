4ac0b7805760bba9e833d4a8d3ea6ac28db44de5
Tong Zhang <zhangt@frib.msu.edu>
2021-12-08 11:35:57 -0500

    FIX: epics-base-tools command.

--- _epics_appimage_apps/__init__.py
+++ _epics_appimage_apps/__init__.py
@@ -12,16 +12,25 @@ LAUNCHER_NAME = 'epics-base-tools'
 PKG_DIR = pathlib.Path(__file__).parent.parent
 APP_NAME_PATH_MAP = {
     f.name.rsplit('-', 1)[0]: f.resolve()
-    for f in PKG_DIR.glob("AppImages/*.AppImage")
+    for f in PKG_DIR.glob(f"_AppImage/{LAUNCHER_NAME}*.AppImage")
 }
 
-r = subprocess.run(APP_NAME_PATH_MAP[LAUNCHER_NAME], capture_output=True, text=True)
+r = subprocess.run([APP_NAME_PATH_MAP[LAUNCHER_NAME], '--list'], capture_output=True, text=True)
 # a list of app names which could be launched from the global entry point
 SUPPORT_APP_LIST = r.stdout.strip().split()
 
 
 class AppRunner(object):
-    def __init__(self, app_name):
+    def __init__(self, app_name=None):
+        if app_name is None:
+            # global entry point
+            self._cmdlist = [APP_NAME_PATH_MAP[LAUNCHER_NAME]]
+        else:
+            # + app name
+            self._cmdlist = [APP_NAME_PATH_MAP[LAUNCHER_NAME], app_name]
+
+    def __call__(self):
+        """Run an AppImage."""
         self._cmdlist = [APP_NAME_PATH_MAP[LAUNCHER_NAME], app_name]
 
     def __call__(self):
@@ -30,9 +39,13 @@ class AppRunner(object):
         subprocess.run(cmd, stderr=subprocess.STDOUT)
 
 
-# create exec functions for each appimage
+# create exec functions for each app
 _fn_name_exec_map = {}  # CLI tool name : entry point function
-for _app in APP_NAME_PATH_MAP:
+for _app in SUPPORT_APP_LIST:
     _fn_name = 'run_' + _app.replace("-", "_")
     globals()[_fn_name] = AppRunner(_app)
     _fn_name_exec_map[_app] = _fn_name
+
+# create exec function for global entry point
+globals()['run_epics_base_tools'] = AppRunner()
+_fn_name_exec_map[LAUNCHER_NAME] = 'run_epics_base_tools'
